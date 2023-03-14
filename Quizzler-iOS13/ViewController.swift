ef2261929a7814aebf48e9a1e90b3d5fbed91ed0
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-03-14 10:10:52 +0100

    Created outlets and a single action for the True and False button.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionText: UILabel!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var treuButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+     
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
